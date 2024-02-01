ed85b72f68a84930804c15afad31e6b4fcf17c79
John Tseng <44330430+JohnTseng95@users.noreply.github.com>
2024-01-31 21:57:44 -0800

    1/30 Lab

--- Assets/PlaceTrackImage.cs
+++ Assets/PlaceTrackImage.cs
@@ -12,13 +12,19 @@ public class PlaceTrackImage : MonoBehaviour
     private ARTrackedImageManager _trackImageManager;
 
     public GameObject[] ArPrefabs;
-    public int trackCount = 0;
-    public TMP_Text counter;
+    public int trackingCount = 0;
+    public int limitedCount = 0;
+    public int nonCount = 0;
+    public TMP_Text trackingNumber;
+    public TMP_Text limitedNumber;
+    public TMP_Text nonNumber;
 
     private readonly Dictionary<string, GameObject> _instantiatedPrefabs = new Dictionary<string, GameObject>();
     void Update()
     {
-        counter.text = trackCount.ToString();
+        trackingNumber.text = trackingCount.ToString();
+        limitedNumber.text = limitedCount.ToString();
+        nonNumber.text = nonCount.ToString();
     }
     void Awake()
     {
@@ -36,7 +42,7 @@ public class PlaceTrackImage : MonoBehaviour
 
     private void OnTrackedImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        trackCount += 1;
+        
         foreach (var trackedImage in eventArgs.added)
         {
             var imageName = trackedImage.referenceImage.name;
@@ -53,12 +59,25 @@ public class PlaceTrackImage : MonoBehaviour
             }
         }
 
-        //foreach(var trackedImage in eventArgs.updated)
-        //{
-        //    _instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
-        //}
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            
+            if(trackedImage.trackingState == TrackingState.Tracking)
+            {
+                trackingCount += 1;
+            }
+            else if(trackedImage.trackingState == TrackingState.Limited)
+            {
+                limitedCount += 1;
+            }
+            else if(trackedImage.trackingState == TrackingState.None)
+            {
+                nonCount += 1;
+            }
+            //_instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
+        }
 
-        foreach(var trackedImage in eventArgs.removed)
+        foreach (var trackedImage in eventArgs.removed)
         {
             Destroy(_instantiatedPrefabs[trackedImage.referenceImage.name]);
             _instantiatedPrefabs.Remove(trackedImage.referenceImage.name);
